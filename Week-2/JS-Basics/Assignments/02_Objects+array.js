f4cd10afb76c21e6163c87c5acc942cafc3e62f6
shiivankiit <shivankit7557@gmail.com>
2026-03-30 21:59:50 +0530

    Object and Array github assignment

---
+++ Week-2/JS-Basics/Assignments/02_Objects+array.js
@@ -0,0 +1,48 @@
+//Sum value in object array.
+//Input:{ food: [10, 20, 30], travel: [5, 15], bills: [40, 60] }.
+//Output:{ food: 60, travel: 20, bills: 100 }.
+
+function sum(user) {
+   //Here we use two nested loop
+   //1:---for..in for nested loop.
+   //2:---for..of or for loop for inner loop.
+   let result={};
+   for(let key in obj){
+    let arr=obj[key];
+    let sum=0;
+    for(let value of arr){
+        //console.log(value);
+        sum=sum+value;
+        result[key]=sum;
+    }
+   }
+   console.log(result);
+}
+var obj = {
+    food: [10, 20, 30],
+    travel: [5, 15],
+    bills: [40, 60]
+}
+sum(obj);
+
+
+//Count word occurence in array.
+//Input:["apple", "banana", "apple", "orange", "banana", "apple"]
+//Output:{ apple: 3, banana: 2, orange: 1 }
+//Here the catch is that our input is array and output is object.
+
+function countword(arr){
+   //for array we use for of loop
+   //and we need to convert array into the object.
+   let result={};
+   for(let value of arr){
+     if(result[value]){
+        result[value]++;
+     } else{
+        result[value]=1;
+     }
+   }
+   return result;
+}
+let arr=["apple", "banana", "apple", "orange", "banana", "apple"];
+console.log(countword(arr));
